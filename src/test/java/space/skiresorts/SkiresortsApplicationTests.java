b67d24d4ab4897be82944d0595c39af9f24b9c3c
Ioana84 <55927744+Ioana84@users.noreply.github.com>
2022-11-30 20:36:31 +0200

    tests removed for faster boot

--- src/test/java/space/skiresorts/SkiresortsApplicationTests.java
+++ src/test/java/space/skiresorts/SkiresortsApplicationTests.java
@@ -3,10 +3,10 @@ package space.skiresorts;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SkiresortsApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
